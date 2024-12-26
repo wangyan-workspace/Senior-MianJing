075d42f923d8235d67822ef329a54ea4b710b9f3
wangyan-workspace <2107014530@qq.com>
2024-12-27 00:33:00 +0800

    二刷算法-数组

--- 算法/数组/12_在排序数组中查找元素的第一个和最后一个位置(leetcode_34).js
+++ 算法/数组/12_在排序数组中查找元素的第一个和最后一个位置(leetcode_34).js
@@ -4,5 +4,55 @@
  * @return {number[]}
  */
 var searchRange = function (nums, target) {
-    
-}
+  // 找左边界
+  const getLeftBorder = (nums, target) => {
+    let left = 0;
+    let right = nums.length - 1;
+    let leftBorder = -2;
+
+    while (left <= right) {
+      let mid = left + ((right - left) >> 1);
+      if (nums[mid] >= target) {
+        right = mid - 1;
+        leftBorder = right;
+      } else {
+        left = mid + 1;
+      }
+    }
+
+    return leftBorder;
+  };
+
+  // 找右边界
+  const getRightBorder = (nums, target) => {
+    let left = 0;
+    let right = nums.length - 1;
+    let rightBorder = -2;
+
+    while (left <= right) {
+      let mid = left + ((right - left) >> 1);
+
+      if (nums[mid] <= target) {
+        left = mid + 1;
+        rightBorder = left;
+      } else {
+        right = mid - 1;
+      }
+    }
+
+    return rightBorder;
+  };
+
+  let leftBorderNum = getLeftBorder(nums, target);
+  let rightBorderNum = getRightBorder(nums, target);
+
+  if (leftBorderNum === -2 || rightBorderNum === -2) {
+    return [-1, -1];
+  }
+
+  if (rightBorderNum - leftBorderNum > 1) {
+    return [leftBorderNum + 1, rightBorderNum - 1];
+  }
+
+  return [-1, -1];
+};
